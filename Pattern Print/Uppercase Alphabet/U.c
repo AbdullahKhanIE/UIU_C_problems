49a57721d5e550d5912e6feda99a0592eeaef1b3
synapse <abdullahkhan.ie.official@gmail.com>
2023-08-01 01:11:49 +0600

    added assignment solution

--- Pattern Print/Uppercase Alphabet/U.c
+++ Pattern Print/Uppercase Alphabet/U.c
@@ -5,15 +5,20 @@ int main()
     int row, column, input;
     printf("Enter The Number of Row: \n");
     scanf("%d", &input);
-    if (input % 2 == 1)
-    {
         for (row = 1; row <= input; row++)
         {
             for (column = 1; column <= input; column++)
             {
-                if ((row <= input / 2 + 1 && row == column) || (row <= input / 2 + 1 && row + column == input + 1))
+                if (row == input || column == input || column == 1)
                 {
-                    printf("V ");
+                    if (row == input && column == 1 || row == input && column == input)
+                    {
+                        printf("  ");
+                    }
+                    else
+                    {
+                        printf("U ");
+                    }
                 }
                 else
                 {
@@ -22,10 +27,5 @@ int main()
             }
             printf("\n");
         }
-    }
-    else
-    {
-        printf("Invalid Input.\nEnter an Odd Number.\n");
-    }
     return 0;
 }
